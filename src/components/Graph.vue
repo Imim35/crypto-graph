b9c836f8aeb2317739a5e4997403b7e372de0812
Evgeniy <geniaimim35@gmail.com>
2021-06-28 01:01:53 +0300

    fix graph close btn

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="graph">
     <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
-    <img class="graph__vector" src="@/assets/Vector.svg" @click="$bus.$emit('graph:close')" alt="vector">
+    <span class="graph__vector"  @click="$bus.$emit('graph:close')" alt="vector"></span>
   </div>
 </template>
 
@@ -45,8 +45,18 @@ export default {
   }
   &__vector {
     position: absolute;
+    height: 12.25px;
+    width: 12.25px;
+    background-image: url("../assets/Vector.svg");
+    background-size: cover;
     right: 0;
     top: 0;
+    transition: 0.3s ease;
+    &:hover {
+      height: 12.25px;
+      width: 12.25px;
+      background-image: url("../assets/close.png");
+    }
   }
 }
 </style>
